4b05fc1a554740b67f68624022516da48eaadd7c
Alexandr Makhin <amakhin@gmail.com>
2026-01-27 09:45:31 +0100

    small fixes

--- SignalBot/Services/Telegram/FatPigParser.cs
+++ SignalBot/Services/Telegram/FatPigParser.cs
@@ -18,16 +18,16 @@ public sealed class FatPigParser : SignalMessageParserBase
           (?<direction>LONG|SHORT)\s*.*?
           Entry:\s*(?<entry1>[\d.]+)\s*(?:-\s*(?<entry2>[\d.]+))?\s*.*?
           Leverage:\s*(?<leverage>\d+)x\s*.*?
-          (?:Target\s*1:\s*(?<t1>[\d.]+)\s*)?
-          (?:Target\s*2:\s*(?<t2>[\d.]+)\s*)?
-          (?:Target\s*3:\s*(?<t3>[\d.]+)\s*)?
-          (?:Target\s*4:\s*(?<t4>[\d.]+)\s*)?
-          (?:Target\s*5:\s*(?<t5>[\d.]+)\s*)?
-          (?:Target\s*6:\s*(?<t6>[\d.]+)\s*)?
-          (?:Target\s*7:\s*(?<t7>[\d.]+)\s*)?
-          (?:Target\s*8:\s*(?<t8>[\d.]+)\s*)?
-          (?:Target\s*9:\s*(?<t9>[\d.]+)\s*)?
-          (?:Target\s*10:\s*(?<t10>[\d.]+)\s*)?.*?
+          (?:.*?Target\s*1:\s*(?<t1>[\d.]+)\s*)?
+          (?:.*?Target\s*2:\s*(?<t2>[\d.]+)\s*)?
+          (?:.*?Target\s*3:\s*(?<t3>[\d.]+)\s*)?
+          (?:.*?Target\s*4:\s*(?<t4>[\d.]+)\s*)?
+          (?:.*?Target\s*5:\s*(?<t5>[\d.]+)\s*)?
+          (?:.*?Target\s*6:\s*(?<t6>[\d.]+)\s*)?
+          (?:.*?Target\s*7:\s*(?<t7>[\d.]+)\s*)?
+          (?:.*?Target\s*8:\s*(?<t8>[\d.]+)\s*)?
+          (?:.*?Target\s*9:\s*(?<t9>[\d.]+)\s*)?
+          (?:.*?Target\s*10:\s*(?<t10>[\d.]+)\s*)?.*?
           StopLoss:\s*(?<sl>[\d.]+)",
         CommonRegexOptions);
 
